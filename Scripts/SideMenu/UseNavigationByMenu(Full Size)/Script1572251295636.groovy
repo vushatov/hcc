93697b6a0a5b2dafe0c6761b62511bf9a88479ab
Vlad <vladyslav.ushatov@gmail.com>
2019-10-28 11:25:13 +0300

    ZzZ

---
+++ Scripts/SideMenu/UseNavigationByMenu(Full Size)/Script1572251295636.groovy
@@ -0,0 +1,57 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.callTestCase(findTestCase('LoginPage/Login by Email'), [('Email') : findTestData('DataForLogin/CredentialsForLogin').getValue(
+            1, 1), ('Password') : findTestData('DataForLogin/CredentialsForLogin').getValue(3, 1)], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementAttributeValue(findTestObject('Object Repository/SideMenu/SizeOfMenu'), 'class', 'navbar-nav bg-gradient-dark sidebar sidebar-dark accordion', 1) //full
+
+
+WebUI.verifyElementPresent(findTestObject('Object Repository/SideMenu/TitleWhenOpenedMenu'), 1)
+
+
+WebUI.click(findTestObject('SideMenu/a_Users'))
+
+CheckURL('http://hcc-asset-server.program-ace.net/admin/users')
+
+WebUI.click(findTestObject('SideMenu/a_User Groups'))
+CheckURL('http://hcc-asset-server.program-ace.net/admin/groups')
+
+WebUI.click(findTestObject('SideMenu/a_Applications'))
+CheckURL('http://hcc-asset-server.program-ace.net/applications')
+
+WebUI.click(findTestObject('SideMenu/a_Assets'))
+CheckURL('http://hcc-asset-server.program-ace.net/assets')
+
+WebUI.click(findTestObject('SideMenu/a_Banned IPs'))
+CheckURL('http://hcc-asset-server.program-ace.net/admin/banned')
+
+WebUI.click(findTestObject('SideMenu/a_Dashboard'))
+CheckURL('http://hcc-asset-server.program-ace.net/admin')
+
+
+
+void CheckURL(String expected) {
+    String actual = WebUI.getUrl()
+
+    if (actual.compareTo(expected) == 0) {
+        System.out.println('Correct page is opened!')
+    } else {
+        assert false : 'Wrong page is opened!!'
+    }
+}
+
---
+++ Test Suites/CheckSideMenu.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CheckSideMenu</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a65083a0-3d14-4a47-9e70-1e24a389901c</testSuiteGuid>
+   <testCaseLink>
+      <guid>ce383b52-ceb8-4c9a-babc-e28e6e675065</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SideMenu/CheckElementsOfSideMenu</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2be19ea3-99c1-43d7-bc30-899ec2472bcc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SideMenu/UseNavigationByMenu(Full Size)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>04e019aa-11b7-46d9-b395-6e264f269157</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SideMenu/UseNavigationByMenu(Minimized)</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
